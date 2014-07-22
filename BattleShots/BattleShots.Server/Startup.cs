1a5392a8d6a76e35415063521b5d592b0d34518d
Yordan Darakchiev <iordan93@gmail.com>
2014-07-22 22:53:09 +0300

    Added initial project structure

---
+++ BattleShots/BattleShots.Server/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(BattleShots.Server.Startup))]
+
+namespace BattleShots.Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
