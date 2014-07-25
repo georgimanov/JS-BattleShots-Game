8867a36c344d5501356670625dbc26801e8aa288
Yordan Darakchiev <iordan93@gmail.com>
2014-07-25 08:52:36 +0300

    Added board model

---
+++ BattleShots/BattleShots.Models/Board.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BattleShots.Models
+{
+    public class Board
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string BoardBody { get; set; }
+    }
+}
---
+++ BattleShots/BattleShots.Server/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace BattleShots.Server.Controllers
+{
+    public class AccountController : ApiController
+    {
+
+    }
+}
