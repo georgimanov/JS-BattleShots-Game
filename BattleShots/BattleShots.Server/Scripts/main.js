d10400a4fb330ef02ced2008bebca40375cf7e10
Yordan Darakchiev <iordan93@gmail.com>
2014-07-30 22:13:37 +0300

    .

--- BattleShots/BattleShots.Server/Scripts/main.js
+++ BattleShots/BattleShots.Server/Scripts/main.js
@@ -2,96 +2,95 @@ var baseUrl = "http://localhost:32033/api/";
 //var baseUrl = "http://battleshots-1.apphb.com/api/";
 var gameAcces = false;
 var userName = localStorage.getItem('userName');
-if(userName == null){
+if (userName == null) {
     userName = "Example User";
 }
-$(document).ready(function(){
+
+$(document).ready(function () {
     checkGameAcces();
 });
 
-function checkGameAcces(){
-    if(localStorage.getItem("sessionKey") == null){
-        $('#dialog').dialog({modal:true});
-    }else{
+function checkGameAcces() {
+    if (localStorage.getItem("sessionKey") == null) {
+        $('#dialog').dialog({ modal: true });
+    } else {
         RunGame();
     }
 }
 
-function RunGame(){
-     userName = localStorage.getItem('userName');
+function RunGame() {
+    userName = localStorage.getItem('userName');
     $('#username').html(userName);
     $('#dialog').dialog('close');
     $('#error-dialog').dialog('close');
 
 }
-function registerUser(){
+function registerUser() {
     var loginName = $('#username-join').val();
     var userPass = $('#password-join').val();
     var pass = SHA1(userPass);
 
     var data = {
-        "username" : loginName,
-        "password" : pass
+        "username": loginName,
+        "password": pass
     };
 
-    httpRequester.postJson(baseUrl+'account/register',data,{})
-        .then(function(data){
-         var DataUserName = data['Username'];
-         var dataSessionKey = data['SessionKey'];
-         localStorage.setItem('userName',DataUserName);
-         localStorage.setItem('sessionKey',dataSessionKey);
+    httpRequester.postJson(baseUrl + 'account/register', data, {})
+        .then(function (data) {
+            var DataUserName = data['Username'];
+            var dataSessionKey = data['SessionKey'];
+            localStorage.setItem('userName', DataUserName);
+            localStorage.setItem('sessionKey', dataSessionKey);
 
-         checkGameAcces();
-            },function(error){
-        showError(error);
+            checkGameAcces();
+        }, function (error) {
+            showError(error);
         });
 
 
 }
-
-function checkLogin(){
+function checkLogin() {
 
     var loginName = $('#username-login').val();
     var userPass = $('#password-login').val();
     var passSha1 = SHA1(userPass);
     var data = {
         'username': loginName,
-        'password':passSha1
+        'password': passSha1
     };
-    httpRequester.postJson(baseUrl+'account/login',data,{})
-        .then(function(data){
+    httpRequester.postJson(baseUrl + 'account/login', data, {})
+        .then(function (data) {
 
             var DataUserName = data['Username'];
             var dataSessionKey = data['SessionKey'];
-            localStorage.setItem('userName',DataUserName);
-            localStorage.setItem('sessionKey',dataSessionKey);
+            localStorage.setItem('userName', DataUserName);
+            localStorage.setItem('sessionKey', dataSessionKey);
 
             checkGameAcces();
-        },function(error){
+        }, function (error) {
             showError(error);
         });
 }
 
-
-function showError(error){
+function showError(error) {
     var responseText = JSON.parse(error['responseText']);
     var text = responseText.Message;
-    $('#error-dialog').html(text).dialog({modal:true});
+    $('#error-dialog').html(text).dialog({ modal: true });
 }
 
-function logOut(){
-    var sk =  localStorage.getItem('sessionKey');
+function logOut() {
+    var sk = localStorage.getItem('sessionKey');
     var headers = {
-        'X-SessionKey' :sk
+        'X-SessionKey': sk
     }
-    httpRequester.putJson(baseUrl+'account/logout',{},headers)
-        .then(function(data){
+    httpRequester.putJson(baseUrl + 'account/logout', {}, headers)
+        .then(function (data) {
             alert('data: ' + data);
             localStorage.removeItem('userName');
             localStorage.removeItem('sessionKey');
             location.reload();
             checkGameAcces();
-        },function(error){
+        }, function (error) {
             showError(error);
         });
 }
